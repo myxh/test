8c11b188176dd44e1ef569d7e03daabaccf8fcf9
GitHub Action <action@github.com>
2022-04-22 07:20:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/myxh/test/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/myxh/test/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.myxh.test.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.myxh.test.MyBundle
 
 class MyApplicationService {
 
